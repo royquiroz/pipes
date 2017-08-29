27cc3cc7b3d2327c75c9f8989f777b67eb792069
Gonzalo Quiroz <kiyegoo@gmail.com>
2017-08-29 16:00:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ 
+  nombre = "Gonzalo";
+
+  nombre2 = "GoNzAlO rOdRiGo QuIrOz gOnZaLeZ"
+
+  arreglo = [1,2,3,4,5,6,7,8,9,10];
+  
+  PI = Math.PI
+  
+  a = 0.234;
+
+  salario = 1234.5;
+
+  heroe = {
+    nombre: "Bruce Wane",
+    clave: "Batman",
+    edad: 52,
+    direccion: {
+      calle: "Privada",
+      casa: "Mansion Wane"
+    }
+  };
+
+valorDePromesa = new Promise( ( resolve, reject) => {
+  setTimeout( () => resolve('Llego la data'), 3500 );
+});
+
+fecha = new Date()
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LOCALE_ID } from "@angular/core";
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from "./pipes/capitalizado.pipe";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide:LOCALE_ID, useValue: "es" }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
